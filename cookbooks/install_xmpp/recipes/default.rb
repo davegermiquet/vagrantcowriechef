f8ca51997fdc1515e44efae600dd4b0e212d8ce0
Dave Germiquet <davegermiquet@trulycanadian.net>
2018-01-01 18:11:29 -0500

    Add XMPP

--- cookbooks/install_xmpp/recipes/default.rb
+++ cookbooks/install_xmpp/recipes/default.rb
@@ -17,3 +17,57 @@ pkg_path = '/tmp/ejabber.rpm'
 rpm_package pkg_path do
   action :install
 end
+
+cookbook_file '/opt/ejabberd/conf/ejabberd.yml' do
+  source 'ejabberd.yml'
+  owner 'ejabberd'
+  group 'ejabberd'
+  mode '0755'
+  action :create
+end
+
+cookbook_file '/opt/ejabberd/conf/ejabberdctl.cfg' do
+  source 'ejabberdctl.cfg'
+  owner 'ejabberd'
+  group 'ejabberd'
+  mode '0755'
+  action :create
+end
+
+execute 'mysql_grant_ejabber' do
+    command  "echo \"CREATE DATABASE ejabberd;GRANT ALL ON ejabberd.* TO 'ejabberd'@'localhost' IDENTIFIED BY 'ejabberd';\" | mysql -u root --password=#{node['mysql']['rootPassword']}"
+   not_if { ::Dir.exist?("/var/lib/mysql/ejabberd")}
+   notifies :run, 'execute[mysql_query_ejabber]', :immediately
+end
+
+execute 'mysql_query_ejabber' do
+   command "echo \"use ejabberd;source /opt/ejabberd-17.12/lib/ejabberd-17.12/priv/sql/mysql.sql;\" | mysql -u ejabberd --password=ejabberd"
+   action :nothing
+end
+
+execute 'moveservicefile'  do
+  command "cp /opt/ejabberd-17.12/bin/ejabberd.service /etc/systemd/system"
+  notifies :run, 'execute[systemctl]', :immediately
+end
+
+execute 'systemctl'  do 
+  command "systemctl daemon-reload"
+  notifies :run, 'execute[startService]', :immediately
+  action :nothing
+end
+
+execute 'startService'  do 
+  command "systemctl start ejabberd"
+  notifies :run, 'execute[AddUser]', :immediately
+  action :nothing
+end
+
+execute 'AddUser' do
+  command "/opt/ejabberd-17.12/bin/ejabberdctl register admin cowrie changeme"
+  action :nothing
+end
+
+service 'ejabberd'  do
+   action :restart
+end
+
